f8b1ae058a77a34bd5a934ac66789e8d4dfdb182
malykhdenis <85461091+malykhdenis@users.noreply.github.com>
2024-05-05 18:00:23 +0300

    redirect to manager page after editing order

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 from django.shortcuts import reverse
 from django.templatetags.static import static
 from django.utils.html import format_html
+from django.utils.http import url_has_allowed_host_and_scheme
 
 from .models import Product
 from .models import ProductCategory
@@ -129,6 +131,18 @@ class OrderAdmin(admin.ModelAdmin):
                 instance.price = instance.product.price
             instance.save()
         formset.save_m2m()
+
+    def response_post_save_change(self, request, obj):
+        res = super().response_post_save_change(request, obj)
+        if "next" in request.GET:
+            redirect_url = request.GET['next']
+            if url_has_allowed_host_and_scheme(
+                redirect_url,
+                allowed_hosts=['localhost'],
+            ):
+                return HttpResponseRedirect(redirect_url)
+        else:
+            return res
 
 
 @admin.register(OrderProduct)
